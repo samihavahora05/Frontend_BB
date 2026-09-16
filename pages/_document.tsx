c7b6d02cb3fcdb6e33f7444faac4375af8774b5b
Your Name <140956680+SamihaVahora@users.noreply.github.com>
2026-09-16 15:29:57 +0530

    Add Google Analytics (gtag.js) and GTM scripts to Next.js Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,69 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const defaultGaId = 'G-7FTZWWKW27';
+  const customGaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || '';
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
+
   return (
     <Html lang="en" data-scroll-behavior="smooth">
       <Head>
         <link rel="icon" href="/Boxxlogo.png" type="image/png" />
         <link rel="shortcut icon" href="/Boxxlogo.png" type="image/png" />
         <link rel="apple-touch-icon" href="/Boxxlogo.png" />
+
+        {/* Google tag (gtag.js) - Primary */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${defaultGaId}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${defaultGaId}');
+              ${customGaId && customGaId !== defaultGaId ? `gtag('config', '${customGaId}');` : ''}
+            `,
+          }}
+        />
+
+        {/* Additional Google tag if dynamic ID is set and distinct */}
+        {customGaId && customGaId !== defaultGaId && (
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${customGaId}`}
+          />
+        )}
+
+        {/* Google Tag Manager */}
+        {gtmId && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');
+              `,
+            }}
+          />
+        )}
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        )}
         <Main />
         <NextScript />
       </body>
